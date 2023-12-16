3ce8e988229d1be5a98bd6c8d40fbecdfb2f3384
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-16 15:04:10 +0000

    commit

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,15 @@
-const reverse = (str) =>{
-  return str.split('').reverse().join('')
-  }
+// const reverse = (str) =>{
+//   return str.split('').reverse().join('')
+//   }
 
-console.log(reverse("hello"))
+// console.log(reverse("hello"))
+
+const reverseString = (str)=>{
+  let reversed = ''
+  for(let i = str.length - 1; i>=0; i --){
+    reversed += str[i]
+  }
+  return reversed
+}
+
+console.log(reverseString("dagim"))
