7eb82bb862c9fae7c85ef0a852c5a0f89b6c9a42
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-16 11:53:25 +0000

    converting the first letter of a sentence or word ot capital lettter

---
+++ titleCase.js
@@ -0,0 +1,10 @@
+function titleCase(str){
+  const words = str.toLowerCase().split(' ')
+  for(let i = 0; i< words.length; i++){
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1)
+  }
+
+  return words.join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
