92340333ccb72478cc86c31b84bb7d80f13c476d
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-16 12:27:37 +0000

    with arrow

--- titleCase.js
+++ titleCase.js
@@ -1,9 +1,8 @@
-function titleCase(str){
-  const words = str.toLowerCase().split(' ')
-
-  for(let i = 0; i < words.length; i++){
-   words[i] = words[i][0].toUpperCase() + words[i].slice(1)
+ const titleCase = (str)=>{
+  const words = str.toLowerCase().split(' ');
+  for(let i = 0; i< words.length; i++){
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1)
   }
-  return words.join(' ');
+  return words.join(' ')
 }
-console.log(titleCase("I'm a little tea pot"));
+console.log(titleCase("kafjsj fff jfajf ffffajf gt"))
