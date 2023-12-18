fc7861de45ec944d4cb2cc052f7e91387fb46c30
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-18 13:04:02 +0000

    recap on palindrome

--- palindrome.js
+++ palindrome.js
@@ -11,12 +11,15 @@
 
 // console.log(isPalin("is palindrome"))
 
+
 function isPalindrome(str){
-  let reversed = ''
-  for(let i = str.length; i>=0; i--){
+  let reversed = "";
+  for(let i = str.length - 1; i>=0; i--){
     reversed += str[i]
   }
   if(reversed === str){
-    console.log("is palindrome!")
+    console.log("is Palindromme!")
   }
 }
+
+console.log(isPalindrome("ispalindrome"))
