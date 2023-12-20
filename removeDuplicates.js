0d23fdd5398d01dfbf2a8dd045c05b033d1f28b4
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-20 19:32:56 +0000

    removing duplicates recap

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,14 +1,11 @@
-let arr = [2,4,3,2,4,5]
-
 let newArr = []
 
 function duplicates(arr){
-   for(let i = 0; i<arr.length; i++){
+  for(let i = 0; i< arr.length; i++){
      if(!newArr.includes(arr[i])){
-       newArr.push(arr[i])
+       newArr.push(arr[i]) //newArr+=arr[i]
      }
-   }
+  }
   return newArr;
 }
-
-console.log(duplicates(arr))
+console.log(duplicates([1,1,2,2,3,4,4,4,4,4,7,7,7,8,8,8,8,8,8,8,8,9]))
