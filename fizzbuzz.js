d1f630cdf0e31437b436fbbf4a8f20e5f059200a
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2024-01-01 19:36:34 +0000

    fizz buzz final

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,6 @@
-for(let i = 1; i <=0; i++){
-  if(i%3 && i%5 === 0){
-    cosole.log("FizzBuzz")
+for(let i = 1; i <=100; i++){
+  if(i%3 === 0 && i%5 === 0){
+    console.log("FizzBuzz")
   }
   else if(i%3 === 0){
     console.log("Fizz")
