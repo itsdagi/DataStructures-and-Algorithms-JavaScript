8c31e62b7c095c76e09c3fb4206d085ebec4f5c9
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2024-01-04 15:58:12 +0000

    array intersection

---
+++ arrayIntersection.js
@@ -0,0 +1,9 @@
+function arrayIntersection(arr1, arr2) {
+  newArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    return arr1[i] === arr2[i];
+  }
+
+}
+console.log(arrayIntersection([1, 2, 3, 4, 5], [1, 2, 3, 4, 6]));
