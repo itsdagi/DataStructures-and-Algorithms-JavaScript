c1d098f42094253e8168ac161bed3480e3480e82
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2024-01-04 16:14:36 +0000

    with length comparing

--- arrayIntersection.js
+++ arrayIntersection.js
@@ -1,9 +1,9 @@
 newArr = [];
 
 function arrayIntersection(arr1, arr2) {
-   // if(arr1.length !== arr2.length){
-   //   return newArr;
-   // }
+   if(arr1.length !== arr2.length){
+     return newArr;
+   }
   for (let i = 0; i < arr1.length; i++) {
    if(arr1[i] === arr2[i]){
      newArr.push(arr1[i])
