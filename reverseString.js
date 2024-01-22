b573d17f49fd469c175f2fd9f7e040381eabc5cb
dagimalemux <dagimalemux@gmail.com>
2024-01-22 12:14:03 +0300

    commit

--- reverseString.js
+++ reverseString.js
@@ -4,11 +4,14 @@
 // console.log(reversed("hello"))
 
 const reversed=(str)=> {
-  let  reversed = "";
+  let  reversed = " ";
    for(let i = str.length -1; i >=0; i--){
-     reversed+= str[i];
+     reversed += str[i];
    }
   return reversed;
 }
 
 console.log(reversed("dagim"))
+
+// write a reverse string without methods
+
